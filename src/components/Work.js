8944b5ae4da0de9de50907bd9ee351b6dcca2a98
sleepless <arnas.manucan@gmail.com>
2023-01-10 17:38:52 +0200

    Add another project to work tab

--- src/components/Work.js
+++ src/components/Work.js
@@ -6,6 +6,7 @@ import project2 from '../assets/project2.png'
 import project3 from '../assets/project3.png'
 import project4 from '../assets/project4.png'
 import project5 from '../assets/project5.png'
+import project6 from '../assets/project6.png'
 import project7 from '../assets/project7.png'
 
 
@@ -23,6 +24,12 @@ export default function Work() {
             <h1 className='text_intro'>All of these projects were done while learning front-end development.</h1>
             <p className='text'>Creating these projects helped me learn and solidify my new-found knowledge on HTML, CSS, JavaScript and React. This personal/portfolio website is my latest finished project created using React.</p>
             <div className='grid'>
+                <Project
+                    key={nanoid()}
+                    img={project6}
+                    title='REST Countries'
+                    url='https://sleep24less.github.io/rest-countries/'
+                    github='https://github.com/sleep24less/rest-countries' />
                 <Project
                     key={nanoid()}
                     img={project7}
